4509413ee2cc37edcf66db8d40fa504dd411b213
guizinsx <guizin51@gmail.com>
2024-03-04 16:35:14 -0300

    receber valores do cadastro, botao salvar e limpar

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Toast
 import com.example.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -50,6 +51,94 @@ class MainActivity : AppCompatActivity() {
 
             }
 
+        }
+        with(amb){
+            salvarBt.setOnClickListener{
+                val nome = amb.nomeEt.text
+                val email = amb.emailEt.text
+                val emailchecked = amb.emailCb.isChecked
+                val telefone = amb.telefoneEt.text
+                val celular = amb.celularEt.text
+                val sexo = if (amb.masculinoRb.isChecked) "Masculino" else "Feminino"
+                val nascimento = amb.nascimentoEt.text
+                val formacao = amb.formacaoSp.selectedItem.toString()
+                val vagas = amb.vagasEt.text
+
+                val infoCadastro = StringBuilder()
+                infoCadastro.append("Nome completo: $nome\n")
+                infoCadastro.append("Email: $email\n")
+                infoCadastro.append("Receber atualização: $emailchecked\n")
+                infoCadastro.append("Telefone: $telefone\n")
+                if (amb.celularCk.isChecked) {
+                    infoCadastro.append("Celular: $celular\n")
+                }
+                infoCadastro.append("Sexo: $sexo\n")
+                infoCadastro.append("Data de nascimento: $nascimento\n")
+                infoCadastro.append("Formação: $formacao\n")
+
+                if (amb.fundmedioformaturaLl.visibility == View.VISIBLE) {
+                    val anoFormatura = amb.anoformaturaEt.text
+                    infoCadastro.append("Ano de formatura (fundamental e medio): $anoFormatura\n")
+                }
+
+                if (amb.gradespecLl.visibility == View.VISIBLE) {
+                    val anoConclusao = amb.anoconclusaoEt.text
+                    val instituicao = amb.instituicaoEt.text
+                    infoCadastro.append("Ano de Conclusão (Graduação): $anoConclusao\n")
+                    infoCadastro.append("Instituição (Graduação): $instituicao\n")
+                }
+
+                if (amb.mestradodoutoradoLl.visibility == View.VISIBLE) {
+                    val anoConclusao2 = amb.anoconclusao2Et.text
+                    val instituicao2 = amb.instituicao2Et.text
+                    val tituloMonografia = amb.tituloEt.text
+                    val orientador = amb.orientadorEt.text
+                    infoCadastro.append("Ano de Conclusão (Mestrado/Doutorado): $anoConclusao2\n")
+                    infoCadastro.append("Instituição (Mestrado/Doutorado): $instituicao2\n")
+                    infoCadastro.append("Título de monografia: $tituloMonografia\n")
+                    infoCadastro.append("Orientador: $orientador\n")
+                }
+                infoCadastro.append("Vagas de interesse: $vagas\n")
+                println(infoCadastro)
+                "HaVagas cadastro: ${infoCadastro}, ".also {
+                    Toast.makeText(
+                        this@MainActivity,
+                        it,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            limparBt.setOnClickListener {
+                amb.nomeEt.text.clear()
+                amb.emailEt.text.clear()
+                amb.emailCb.isChecked = false
+                amb.telefoneEt.text.clear()
+                amb.comercialRb.isChecked = false
+                amb.residencialRb.isChecked = false
+                amb.celularCk.isChecked = false
+                amb.celularEt.text.clear()
+                amb.masculinoRb.isChecked = false
+                amb.femininoRb.isChecked = false
+                amb.nascimentoEt.text.clear()
+                amb.formacaoSp.setSelection(0)
+                amb.vagasEt.text.clear()
+
+                if (amb.fundmedioformaturaLl.visibility == View.VISIBLE) {
+                    amb.anoformaturaEt.text.clear()
+                }
+
+                if (amb.gradespecLl.visibility == View.VISIBLE) {
+                    amb.anoconclusaoEt.text.clear()
+                    amb.instituicaoEt.text.clear()
+                }
+
+                if (amb.mestradodoutoradoLl.visibility == View.VISIBLE) {
+                    amb.anoconclusao2Et.text.clear()
+                    amb.instituicao2Et.text.clear()
+                    amb.tituloEt.text.clear()
+                    amb.orientadorEt.text.clear()
+                }
+            }
         }
 
     }
