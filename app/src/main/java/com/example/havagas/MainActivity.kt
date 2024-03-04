c73d92044877f6220fab3218e2ca3428eabaa9f1
guizinsx <guizin51@gmail.com>
2024-03-04 14:31:21 -0300

    criando activity main

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.havagas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var amb: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+
+
     }
 }
